6edc902c25f794c09ccdbc17e3e5513321a91499
Morgane Broutet <morgane.broutet@gmail.com>
2019-12-17 23:11:47 +0100

    add base project

---
+++ src/components/list.js
@@ -0,0 +1,40 @@
+"use-strict";
+
+import React from 'react';
+
+class List extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            inputValue: "",
+            tasks: []
+        };
+
+        // vos bind etc...
+        this.toggleChange = this.toggleChange.bind(this);
+    }
+
+    toggleChange(event) {
+        
+    }
+
+    render() {
+        return (
+            <div>
+                <input type="text" value={this.state.inputValue} onChange={this.toggleChange}/>
+                <button type="button" onClick={this.addTask}>Add task</button>
+                {
+                    this.state.tasks.map((task, index) => {
+                        return (
+                            <p>{task}</p>
+                        )
+                    })
+                }
+            </div>
+        );
+    }
+}
+
+export default List;
