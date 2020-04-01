82cae27b807a5e3bc8b76e5ea05d4d9f992cee77
Morgane Broutet <morgane.broutet@gmail.com>
2020-04-01 15:22:01 +0200

    add persistent storage

--- src/components/List.js
+++ src/components/List.js
@@ -1,21 +1,16 @@
-import React from 'react';
-import Todo from './Todo';
+import React from "react";
+import Todo from "./Todo";
 
 const List = ({ toDos, handleDelete }) => {
-    
-    return(
-        <div>
-            <ul>
-                {toDos.map(todo => (
-                    <Todo 
-                    key={todo.id}
-                    contentToDo={todo}
-                    handleDelete={handleDelete}
-                    />
-                ))}      
-            </ul>
-        </div>
-    );
-}
+  return (
+    <div>
+      <ul>
+        {toDos.map(todo => (
+          <Todo key={todo.id} contentToDo={todo} handleDelete={handleDelete} />
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default List;
